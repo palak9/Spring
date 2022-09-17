df37c9d539ca3b13c63f3faab92cb746a48259f8
Palak Rathi <palakrathi09@gmail.com>
2022-09-17 14:24:16 +0530

    Config setter injection type

--- src/main/java/com/spring/springcore/App.java
+++ src/main/java/com/spring/springcore/App.java
@@ -6,10 +6,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
 	public static void main(String[] args) {
 
-		System.out.println("Setter Injection..");
+		System.out.println("Setter Injection :: 1");
 		ApplicationContext setterContext = new ClassPathXmlApplicationContext("setter-injection.xml");
 		Student student1 = (Student) setterContext.getBean("student1");
 		System.out.println(student1.toString());
 
+		System.out.println("Setter Inejction :: 2");
+		ApplicationContext setterContext2 = new ClassPathXmlApplicationContext("setter-injection.xml");
+		Student student2 = (Student) setterContext2.getBean("student2");
+		System.out.println(student2.toString());
+		
 	}
 }
