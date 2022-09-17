4a46b877ce30ec832c1568575f779f62709ad0bd
Palak Rathi <palakrathi09@gmail.com>
2022-09-17 13:13:05 +0530

    Setter Injection

--- src/main/java/com/spring/springcore/App.java
+++ src/main/java/com/spring/springcore/App.java
@@ -1,13 +1,15 @@
 package com.spring.springcore;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("Setter Injection..");
+		ApplicationContext setterContext = new ClassPathXmlApplicationContext("setter-injection.xml");
+		Student student1 = (Student) setterContext.getBean("student1");
+		System.out.println(student1.toString());
+
+	}
 }
